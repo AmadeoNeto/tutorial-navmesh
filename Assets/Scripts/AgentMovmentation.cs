98fab6f473d258656bebbf7de5a57ad368b42526
AmadeoNeto <amadeotatocota@gmail.com>
2019-09-22 10:27:44 -0300

    Movimentation Scripts

---
+++ Assets/Scripts/AgentMovmentation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovmentation : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Vector3 destination;
+
+    void FixedUpdate()
+    {
+        agent.SetDestination(destination);
+    }
+}
---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+
+    void FixedUpdate()
+    {
+        agent.SetDestination(target.position);
+    }
+}
